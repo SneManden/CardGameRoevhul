4f0a25328f0b95275b8ad22af4c125b0efd2edfc
SneManden <casper@snemanden.com>
2026-01-14 20:39:17 +0100

    Initial commit

---
+++ main.ts
@@ -0,0 +1,6 @@
+import { Game } from "./game.ts";
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  new Game().run();
+}
---
+++ main_test.ts
@@ -0,0 +1,5 @@
+import { assertEquals } from "@std/assert";
+
+Deno.test(function foo() {
+  // TODO: implement
+});
